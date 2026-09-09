72f1cfcbc89b0646acd4f2f4ae9c1c4dedbc39b9
ArnavGupta <arnavgupta052006@gmail.com>
2026-09-09 16:34:35 +0530

    Made auth.route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const googleAuth = async (req, res) => {
--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-
 import dns from "dns";
 // Change DNS
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http:localhost:5173",
+    credentials:true
+}))
+app.use("/api/auth" , authRouter)
+
 
 app.listen(port, () => {
   console.log("server started");
-  connectDB();
+  connectDB()
 });
