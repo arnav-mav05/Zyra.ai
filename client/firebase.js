4e3c4805bca9861601f41c6dcb3aa04958b3b2e5
ArnavGupta <arnavgupta052006@gmail.com>
2026-09-16 03:35:49 +0530

    Add zrya client frontend

---
+++ client/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAo05vvUIaLzfEbanAQE-71EbmRrbzBYvA",
+  authDomain: "zyraai-7629f.firebaseapp.com",
+  projectId: "zyraai-7629f",
+  storageBucket: "zyraai-7629f.firebasestorage.app",
+  messagingSenderId: "340293324021",
+  appId: "1:340293324021:web:7bfa57ce3698fd7bd489bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
